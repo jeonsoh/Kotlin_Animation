693df95b676e777ec2b1e3b6bf4fa2c33558fb9b
jeonsoh <SoHyang.Jeon126@gmail.com>
2018-07-23 17:19:01 +0900

    OnTouchListener for LongClick

--- app/src/main/java/com/example/jeonsohh/paige_animation/MainActivity.kt
+++ app/src/main/java/com/example/jeonsohh/paige_animation/MainActivity.kt
@@ -1,22 +1,17 @@
 package com.example.jeonsohh.paige_animation
 
-import android.content.Context
+import android.animation.AnimatorInflater
+import android.animation.AnimatorSet
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.view.animation.AnimationUtils
-import android.support.v4.app.Fragment
-import android.support.v4.app.FragmentManager
-import android.support.v4.app.FragmentStatePagerAdapter
+import android.os.Handler
+import android.os.Message
 import android.support.v4.view.PagerAdapter
-import android.text.Layout
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 import kotlinx.android.synthetic.main.activity_viewpager_item.view.*
 
-import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -28,26 +23,28 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    class myViewPagerAdapter() : PagerAdapter(){
+    class myViewPagerAdapter() : PagerAdapter() {
+        // var anim: Animation? = null
+
         //소분류
-        var subBubble = listOf<smallItem>( smallItem(R.drawable.image1_bubble1,"image1_bubble1"),
+        var subBubble = listOf<smallItem>(smallItem(R.drawable.image1_bubble1, "image1_bubble1"),
                 smallItem(R.drawable.image1_bubble2, "image1_bubble2"),
-                smallItem(R.drawable.image1_bubble3,"image1_bubble3") )
+                smallItem(R.drawable.image1_bubble3, "image1_bubble3"))
         //대분류
         var bubble = bigItem(R.drawable.image1, "image1", subBubble)
 
-        var subBridge = listOf<smallItem>( smallItem(R.drawable.image2_bridge1, "image2_bridge1"),
-                smallItem(R.drawable.image2_bridge2, "image2_bridge2") )
+        var subBridge = listOf<smallItem>(smallItem(R.drawable.image2_bridge1, "image2_bridge1"),
+                smallItem(R.drawable.image2_bridge2, "image2_bridge2"))
         var bridge = bigItem(R.drawable.image2, "image2", subBridge)
 
-        var items = arrayOf( bubble, bridge)
+        var items = arrayOf(bubble, bridge)
 
         override fun isViewFromObject(view: View, `object`: Any): Boolean {
             return view.equals(`object`)
         }
 
         override fun getCount(): Int {
-            return  items.size
+            return items.size
         }
 
         override fun instantiateItem(container: ViewGroup, position: Int): Any {
@@ -57,12 +54,69 @@ class MainActivity : AppCompatActivity() {
                 m_textview_on_slidingdrawer.setText(items[position].main_text) //기사 내용. 수정
             }
 
-            var anim = AnimationUtils.loadAnimation(container.context, R.anim.zoom_in)
-            view.m_viewpager_imageview.startAnimation(anim)
+            var anim_zoomin = AnimatorInflater.loadAnimator(container.context, R.animator.zoom_in) as AnimatorSet  //property animation방식
+            anim_zoomin.setTarget(view.m_viewpager_imageview)
+            anim_zoomin.start()
+
+            view.m_viewpager_imageview.setOnTouchListener(object : View.OnTouchListener {
+                override fun onTouch(v: View?, m: MotionEvent?): Boolean {
+                    var cordx: Float? = null
+                    var cordy: Float? = null
+                    val LONGPRESS_TIMEOUT  = ViewConfiguration.getLongPressTimeout()
+                    val TAP_TIMEOUT = ViewConfiguration.getTapTimeout()
+                    // Long Press 판단하는 time 가져옴
+                    var touchslop: Int = ViewConfiguration.get(v!!.context).scaledTouchSlop //drag범위 인식하는 기본값
+                    val longClickHandler = object : Handler() {
+                        override fun handleMessage(msg: Message?) {
+                            println(" anim pause??????")
+                            anim_zoomin.pause()
+                        }
+                    }
+                  //  val longClickHandler = Handler()
+//                    val longClickRunnable =  object: Runnable {
+//                        override fun run() {
+//                            println(" anim pause??????")
+//                            anim_zoomin.pause()
+//                        }
+//                    }
+
+                    when (m!!.action) {
+                        MotionEvent.ACTION_DOWN -> { //초기 위치와 시간 기억
+                            cordx = m.x
+                            cordy = m.y
+                            println("Action down....")
+                            //longClickHandler.postDelayed(longClickRunnable, LONGPRESS_TIMEOUT.toLong())
+                            longClickHandler.sendEmptyMessageAtTime(0, 7000)
+                            return true
+                        }
+
+                        MotionEvent.ACTION_UP -> { // 마우스 up
+                            println("Action UP!!! ")
+                            anim_zoomin.resume()
+                            //longClickHandler.removeCallbacks(longClickRunnable)
+                            longClickHandler.removeMessages(0)
+                            return true
+                        }
+                        MotionEvent.ACTION_MOVE -> { //일정범위
+                            println("Action Move!!! ")
+
+
+                        }
+                        else -> {
+
+                        }
+                    }
+
+                    return false
+                }
+            })
 
             container.addView(view)
             return view
         }
+
+
+
 
     }
 
