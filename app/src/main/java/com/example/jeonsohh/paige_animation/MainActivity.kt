6477cf6d3ba60722db5070842a469bca0a2e12de
jeonsoh <SoHyang.Jeon126@gmail.com>
2018-07-26 17:08:59 +0900

    move animation functions to Fragment and solved progressbar animation

--- app/src/main/java/com/example/jeonsohh/paige_animation/MainActivity.kt
+++ app/src/main/java/com/example/jeonsohh/paige_animation/MainActivity.kt
@@ -1,35 +1,54 @@
 package com.example.jeonsohh.paige_animation
 
-import android.animation.AnimatorInflater
 import android.animation.AnimatorSet
-import android.os.*
-import android.support.v7.app.AppCompatActivity
-import android.support.v4.view.PagerAdapter
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentStatePagerAdapter
 import android.support.v4.view.ViewPager
-import android.view.*
-import android.widget.LinearLayout
-import android.widget.ProgressBar
-import android.widget.RelativeLayout
-import android.widget.Toast
+import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.activity_viewpager_item.*
+import java.io.Serializable
 
 
 class MainActivity : AppCompatActivity() {
 
-    companion object {
-        lateinit var mprogressbarAsync : progressAsyncTask
-        lateinit var mprogressbarTimer : progressbarTimer
-        lateinit var mprogressbarThread : progressbarThread
-    }
+    var mAdapter: ViewPagerAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        supportActionBar!!.hide()
 
-        m_viewpager.adapter = myViewPagerAdapter()
-        m_viewpager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener{
+        //소분류
+        var subBubble = listOf<smallItem>(smallItem(R.drawable.image1_bubble1, "image1_bubble1"),
+                smallItem(R.drawable.image1_bubble2, "image1_bubble2"),
+                smallItem(R.drawable.image1_bubble3, "image1_bubble3"))
+        //대분류
+        var bubble = bigItem(R.drawable.image1, "image1", subBubble)
+
+        var subBridge = listOf<smallItem>(smallItem(R.drawable.image2_bridge1, "image2_bridge1"),
+                smallItem(R.drawable.image2_bridge2, "image2_bridge2"))
+        var bridge = bigItem(R.drawable.image2, "image2", subBridge)
+
+        var items: MutableList<ViewerFragment> = arrayListOf()
+
+        val bundle1 = Bundle()
+        val fragment1 = ViewerFragment()
+        bundle1.putSerializable("items", bubble)
+        fragment1.arguments = bundle1
+        items.add(fragment1)
+
+        val bundle2 = Bundle()
+        val fragment2 = ViewerFragment()
+        bundle2.putSerializable("items", bridge)
+        fragment2.arguments = bundle2
+        items.add(fragment2)
+
+
+        mAdapter = ViewPagerAdapter(items, supportFragmentManager)
+        viewpager_main.adapter = mAdapter
+        viewpager_main.offscreenPageLimit = 2
+        viewpager_main.addOnPageChangeListener(object : ViewPager.OnPageChangeListener{
             override fun onPageScrollStateChanged(state: Int) {
             }
 
@@ -37,122 +56,33 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onPageSelected(position: Int) {
-                println("viewpager child count : " + m_viewpager.childCount)
-
-                var progressStatus = 0
-                mprogressbarAsync = progressAsyncTask(position)
-                mprogressbarAsync.execute()
-
-                mprogressbarTimer = progressbarTimer(position,7000,1000)
-             //   mprogressbarTimer.start()
-
-                mprogressbarThread = progressbarThread(position)
-          //      mprogressbarThread.start()
+                var fragment = mAdapter?.getItem(position) as ViewerFragment
+                fragment.animationStart()
             }
 
 
         })
+
+
     }
 
-    override fun onStart() {
-        super.onStart()
+    class ViewPagerAdapter(val items: MutableList<ViewerFragment>, fm: FragmentManager) : FragmentStatePagerAdapter(fm) {
 
-        mprogressbarAsync = progressAsyncTask(0)
-        mprogressbarAsync.execute()
-    }
-
-    inner class progressAsyncTask(position : Int) : AsyncTask<Void, Void, Void>(){
-        var progressStatus = 0
-        var position = position
-
-        override fun doInBackground(vararg p0: Void?): Void? {
-            var temp = m_viewpager.getChildAt(position).findViewWithTag("progressbar"+0) as? ProgressBar
-            println("temp class : " + temp?.javaClass)
-
-            while (progressStatus <= 100){
-                progressStatus++
-                runOnUiThread(Runnable {
-                    temp?.setProgress(progressStatus)
-                })
-                Thread.sleep(100)
-            }
-            return null
+        //페이지 갯수 세는 부분
+        override fun getCount(): Int {
+            return items.size
         }
 
-        override fun onPostExecute(result: Void?) {
-
-            super.onPostExecute(result)
+        //화면 넣어주는 부분
+        override fun getItem(position: Int): Fragment? {
+            return items[position]
         }
 
     }
 
-    inner class progressbarTimer(position:Int, millisInFuture: Long, countDownInterval: Long) : CountDownTimer(millisInFuture, countDownInterval) {
-        var progressStatus = 0
-        var position = position
-
-        override fun onFinish() {
-            println("Timer finished")
-        }
-
-        override fun onTick(p0: Long) {
-
-            var temp = m_viewpager.getChildAt(position).findViewWithTag("progressbar"+0) as? ProgressBar
-            progressStatus++
-            temp?.setProgress(progressStatus*100/7)
-            Thread.sleep(100)
-        }
-
-    }
-
-    inner class progressbarThread(position: Int) : Thread() {
-        lateinit var handler : Handler
-        var progressStatus = 0
-        var position = position
-
-    //    private var mStop : Boolean = false
-   //     private var mPause : Boolean = false
-        private val lock = java.lang.Object()
-
-        var temp = m_viewpager.getChildAt(position).findViewWithTag("progressbar"+0) as? ProgressBar
-
-        override fun run() {
-      /*      while(!mStop){
-                synchronized(this){
-                    while(mPause){
-                        lock.wait()
-                    }
-                }
-        */        Looper.prepare()
-                println("Thread run...")
-                handler = object : Handler() {
-                    override fun handleMessage(msg: Message?) {
-                        progressStatus++
-                        temp?.setProgress(progressStatus)
-                        Thread.sleep(100)
-                    }
-                }
-                Looper.loop()
-
-          //  }
-        }
-
-        public fun stopThread() {
-           // mStop = true
-        }
-        public fun pauseThread(){
-            synchronized(lock){
-                lock.wait()
-            }
-          //  mPause = true
-        }
-        public fun resumeThread(){
-            synchronized(lock){
-            //    mPause = false
-                lock.notify()
-            }
-        }
-
-    }
+    data class bigItem(var main_image: Int, var main_text: String, var subitem: List<smallItem>) : Serializable
+    data class smallItem(var main_image: Int, var main_text: String)
 
 
 }
+
--- app/src/main/java/com/example/jeonsohh/paige_animation/ViewerFragment.kt
+++ app/src/main/java/com/example/jeonsohh/paige_animation/ViewerFragment.kt
@@ -1,61 +1,76 @@
 package com.example.jeonsohh.paige_animation
 
 
+import android.animation.Animator
+import android.animation.AnimatorInflater
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.LinearInterpolator
 import android.widget.LinearLayout
 import android.widget.ProgressBar
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_viewpager_item.*
 import kotlinx.android.synthetic.main.activity_viewpager_item.view.*
 
 
 class ViewerFragment : Fragment(){
+    var currentSubItem : Int = 0
+    lateinit var animatorSet : AnimatorSet
+    lateinit var items : MainActivity.bigItem
+
+    companion object {
+        val TAG = "ViewerFragment"
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        var view = inflater.inflate(R.layout.activity_viewpager_item, container, false)
+        Log.d(TAG, "=== onCreateView ===")
+        return inflater.inflate(R.layout.activity_viewpager_item, container, false)
+    }
 
-        var items = arguments!!.getSerializable("items") as MainActivity2.bigItem
-        println(items.main_text)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        Log.d(TAG, "=== onViewCreated ===")
 
-        view.run {
-            m_viewpager_imageview.setTag("imageview")
-            m_viewpager_imageview.setImageResource(items.main_image) //head이미지
-            m_textview_on_imageview.setText(items.main_text) //기사 head
-            m_textview_on_slidingdrawer.setText(items.main_text) //기사 내용. 수정
-        }
+        items = arguments!!.getSerializable("items") as MainActivity.bigItem
+        Log.e(TAG, items.main_text)
+
+        imageview_viewpager.setImageResource(items.main_image) //head이미지
+        textview_viewpager.setText(items.main_text) //기사 head
+        m_textview_on_slidingdrawer.setText(items.main_text) //기사 내용. 수정
 
         /* ProgressBar 만들기 */
         val subitemSize = items.subitem.size
-        for( i in 0..subitemSize -1) {
+        for( i in 0..(subitemSize-1)) {
             val progressBar = ProgressBar(context, null, android.R.attr.progressBarStyleHorizontal).apply {
                 max = 100
                 setTag("progressbar"+i)
             }
-            val progressbarParams = LinearLayout.LayoutParams(container!!.measuredWidth / subitemSize, ViewGroup.LayoutParams.MATCH_PARENT). apply {
+            val progressbarParams = LinearLayout.LayoutParams(progresslayout_viewpager!!.measuredWidth / subitemSize, ViewGroup.LayoutParams.MATCH_PARENT). apply {
                 setMargins(10,0,10,0)
                 weight = 1F
             }
-            view.m_progresslayout!!.addView(progressBar,i, progressbarParams)
+            progresslayout_viewpager!!.addView(progressBar,i, progressbarParams)
         }
-        println("child? " + view.m_progresslayout.childCount)
+        println("child? " + view.progresslayout_viewpager.childCount)
 
         /* longClick 애니메이션 정지*/
-        view.m_viewpager_imageview.setOnLongClickListener(object : View.OnLongClickListener{
+        imageview_viewpager.setOnLongClickListener(object : View.OnLongClickListener{
             override fun onLongClick(p0: View?): Boolean {
                 println("long click")
-                MainActivity2.animatorSet.pause()
-             //   MainActivity2.anim_zoomin.cancel()
-
+                animatorSet.pause()
                 view.m_pausedlayout.visibility = View.VISIBLE
                 return true
             }
         })
 
         /* onTouch 애니메이션 재시작 */
-        view.m_viewpager_imageview.setOnTouchListener(object : View.OnTouchListener {
+        imageview_viewpager.setOnTouchListener(object : View.OnTouchListener {
             override fun onTouch(v: View?, m: MotionEvent?): Boolean {
 
                 when (m!!.action) {
@@ -64,9 +79,7 @@ class ViewerFragment : Fragment(){
                     }
                     MotionEvent.ACTION_UP -> { // 마우스 up
                         println("Action UP!!! ")
-                        MainActivity2.animatorSet.resume()
-                      //  MainActivity2.anim_zoomin.resume()
-
+                        animatorSet.resume()
                         view.m_pausedlayout.visibility = View.INVISIBLE
                         return true
                     }
@@ -78,8 +91,63 @@ class ViewerFragment : Fragment(){
             }
         })
 
-        return view
+
     }
 
+    fun animationStart() {
+    //    println("viewpager child count : " + viewpager_main.childCount)
 
+        animatorSet = setAnimation(currentSubItem)
+
+        animatorSet.addListener(object : Animator.AnimatorListener{
+            override fun onAnimationRepeat(p0: Animator?) {
+            }
+
+            override fun onAnimationCancel(p0: Animator?) {
+            }
+
+            override fun onAnimationStart(p0: Animator?) {
+                Log.d( TAG, " === Start Animation ===  " )
+
+            }
+
+            override fun onAnimationEnd(p0: Animator?) {
+                Log.d( TAG, " === End Animation ===  " )
+
+                Log.d( TAG, "current sub item num : " + currentSubItem)
+                var childCount = progresslayout_viewpager.childCount
+                Log.d( TAG,"-----Child : " +progresslayout_viewpager.javaClass)
+                Log.d( TAG,"childCount : " + childCount)
+                if (currentSubItem < childCount-1){
+                    currentSubItem++
+                    imageview_viewpager.setImageResource(items.subitem[currentSubItem].main_image)
+                    animationStart()
+               }else{
+                    currentSubItem = 0
+                }
+
+
+            }
+
+        })
+        animatorSet.start()
+
+    }
+
+    fun setAnimation(currentSub : Int) : AnimatorSet {
+        var mImageview = imageview_viewpager
+        var mProgressbar = progresslayout_viewpager!!.findViewWithTag<ProgressBar>("progressbar"+currentSub) as ProgressBar
+
+        var anim_progressbar = ObjectAnimator.ofInt(mProgressbar, "progress", 0, 100).apply {
+            setDuration(7000)
+            setInterpolator(LinearInterpolator())
+        } //progressbar animation
+
+        var anim_zoomin = AnimatorInflater.loadAnimator(mImageview.context, R.animator.zoom_in) as AnimatorSet  //property animation방식
+        anim_zoomin.setTarget(mImageview)
+
+        anim_zoomin.playTogether(anim_progressbar)
+
+        return anim_zoomin
+    }
 }
